2183a0dee0f3115920b4bcc3c3f15e8c26ad9353
Bar Silver <bar.silver@anaplan.com>
2023-12-11 10:31:04 +0200

    BanditManager.run_tests() takes 1 positional argument but 2 were given

--- secure-lambda-scanner.py
+++ secure-lambda-scanner.py
@@ -1,12 +1,16 @@
 import boto3
 import logging
-import bandit
+from bandit.core import config as bandit_config
+from bandit.core import manager as bandit_manager
 from botocore.exceptions import ClientError
 
 
 def main():
     ec2_client = boto3.client('ec2')
     regions = ec2_client.describe_regions()
+
+    config = bandit_config.BanditConfig()
+    manager = bandit_manager.BanditManager(config, "Full")
 
     for region in regions['Regions']:
         lambda_client = boto3.client('lambda', region_name=region['RegionName'])
@@ -15,6 +19,8 @@ def main():
             if 'python' in function['Runtime']:
                 code_url = lambda_client.get_function(FunctionName=function['FunctionName'])['Code']['Location']
                 print(function['FunctionName'], code_url, region['RegionName'], '', sep='\n')
+                #issues = manager.run_tests(code_url)
+
 
 
 
