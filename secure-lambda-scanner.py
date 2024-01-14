2cd09955d174bbaed3d7267ec9537940db83c5c0
Bar Silver <bar.silver@anaplan.com>
2024-01-14 20:50:23 +0200

    Run bandit on each function

--- secure-lambda-scanner.py
+++ secure-lambda-scanner.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import boto3
 import requests
 import zipfile
@@ -5,16 +7,18 @@ import logging
 import io
 import os
 import bandit
-#from bandit.core import config as bandit_config
-#from bandit.core import manager as bandit_manager
 from botocore.exceptions import ClientError
+
+import shutil
+import subprocess
 
 
 def main():
     ec2_client = boto3.client('ec2')
     regions = ec2_client.describe_regions()
 
-    cmd = 'bandit -r '
+    # Find the location of the bandit executable
+    bandit_path = shutil.which('bandit')
 
     for region in regions['Regions']:
         lambda_client = boto3.client('lambda', region_name=region['RegionName'])
@@ -22,7 +26,6 @@ def main():
         for function in functions['Functions']:
             if 'python' in function['Runtime']:
                 code_url = lambda_client.get_function(FunctionName=function['FunctionName'])['Code']['Location']
-                #print(function['FunctionName'], code_url, region['RegionName'], '', sep='\n')
                 destination_folder = function['FunctionName']
                 url_response = requests.get(code_url)
                 if url_response.status_code == 200:
@@ -30,7 +33,14 @@ def main():
                     zip_file.extractall(destination_folder)
                     print(f"Downloaded and extracted to {destination_folder}")
 
-                    print(os.system(f"bandit -r {destination_folder} -o {destination_folder}/out.csv"))
+                    if bandit_path is not None:
+                        # Run bandit as a subprocess
+                        bandit_cmd = f'{bandit_path} -r {destination_folder} -o {destination_folder}/out.csv'
+                        result = subprocess.run(bandit_cmd, shell=True)
+
+                    else:
+                        print("Error: bandit executable not found. Make sure it is installed and in your system's PATH.")
+
                 else:
                     print(f"Failed to download. Status code: {url_response.status_code}")
 
