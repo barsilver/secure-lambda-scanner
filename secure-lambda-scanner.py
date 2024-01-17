1b9220a5d91d13f5fe4d5349d4f3d749ac7d296e
Bar Silver <bar.silver@anaplan.com>
2024-01-17 12:22:06 +0200

    Clean directory after bandit run

--- secure-lambda-scanner.py
+++ secure-lambda-scanner.py
@@ -6,12 +6,15 @@ import zipfile
 import logging
 import io
 from botocore.exceptions import ClientError
-
+import click
 import shutil
 import subprocess
 
+@click.command()
+# Set destination path to store the bandit results files
+@click.option('--destination-directory', '-d', '--dest', required=True, type=str)
 
-def main():
+def main(destination_directory):
     ec2_client = boto3.client('ec2')
     regions = ec2_client.describe_regions()
 
@@ -33,11 +36,12 @@ def main():
 
                     if bandit_path is not None:
                         # Run bandit as a subprocess
-                        bandit_cmd = f'{bandit_path} -r {destination_folder} -o {destination_folder}/out.csv'
+                        bandit_cmd = f'{bandit_path} -r {destination_folder} -f json -o /{destination_directory}/{destination_folder}.json'
                         result = subprocess.run(bandit_cmd, shell=True)
 
                     else:
                         print("Error: bandit executable not found. Make sure it is installed and in your system's PATH.")
+                    shutil.rmtree(destination_folder)
 
                 else:
                     print(f"Failed to download. Status code: {url_response.status_code}")
